d6ddd2635f9353823631152e84036648f8eb4e1e
kebsmirabel <kebinmirabel@gmail.com>
2025-12-05 12:15:25 +0800

    Test simplified app to debug deployment issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,12 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
-import HospitalLogin from "./pages/HospitalLogin";
-import HospitalDashboard from "./pages/HospitalDashboard";
-import HospitalRequestData from "./pages/HospitalRequestData";
-import HospitalAcceptedRequests from "./pages/HospitalAcceptedRequests";
-import HospitalPatientDetail from "./pages/HospitalPatientDetail";
-import HospitalProtectedRoute from "./components/HospitalProtectedRoute";
 
 function App() {
   return (
-    <Routes>
-      {/* Public routes */}
-      <Route path="/" element={<HospitalLogin />} />
-
-      {/* Protected Hospital routes */}
-      <Route
-        path="/hospital-dashboard"
-        element={
-          <HospitalProtectedRoute>
-            <HospitalDashboard />
-          </HospitalProtectedRoute>
-        }
-      />
-      <Route
-        path="/hospital-request-data"
-        element={
-          <HospitalProtectedRoute>
-            <HospitalRequestData />
-          </HospitalProtectedRoute>
-        }
-      />
-      <Route
-        path="/hospital-accepted-requests"
-        element={
-          <HospitalProtectedRoute>
-            <HospitalAcceptedRequests />
-          </HospitalProtectedRoute>
-        }
-      />
-      <Route
-        path="/hospital-accepted-request/:patient_id"
-        element={
-          <HospitalProtectedRoute>
-            <HospitalPatientDetail />
-          </HospitalProtectedRoute>
-        }
-      />
-    </Routes>
+    <div style={{ padding: '20px' }}>
+      <h1>MediVault Hospital</h1>
+      <p>If you can see this, the basic React app is working!</p>
+      <p>Testing deployment...</p>
+    </div>
   );
 }
 
